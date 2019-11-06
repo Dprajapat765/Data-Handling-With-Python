cf9b248ce632be03c4b6c138ffe483345c453b7a
Dinesh Prajapat <45159995+Dprajapat765@users.noreply.github.com>
2019-11-06 12:54:46 +0530

    SQL and No-SQL Database Handling using Python

---
+++ Day_7_Database_handling/database_sqlite3_handling.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 12:50:55 2019
+
+@author: BSDU ADMIN
+"""
+
+"""
+Database handling using sqlite 
+"""
+
+# import the necessary modules
+import sqlite3 as sql
+from pandas import DataFrame
+
+# create a connection to the database file ()
